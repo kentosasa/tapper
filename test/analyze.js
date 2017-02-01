b2e81c7763c727be83ba62def1033ba1108a1b9f
kento sasamoto <knote0721@gmail.com>
2017-02-01 12:51:03 +0900

    add test

--- test/analyze.js
+++ test/analyze.js
@@ -3,8 +3,26 @@ var assert = chai.assert;    // Using Assert style
 var expect = chai.expect;    // Using Expect style
 var should = chai.should();
 var tapper = require('../');
+
 describe('analyze', function() {
+
+  var callback = function (type) {
+    console.log(type)
+  }
+  var analyze = new tapper.analyze(callback)
+
   it('init', function () {
-    var analyze = tapper.analyze
+    var analyze = new tapper.analyze(callback)
   })
+
+  it('load', function () {
+    var knockData = require('./data/knock')
+    analyze.load(knockData.waves[0])
+  })
+
+  it('knock dectation', function () {
+    var waveData = require('./data/knock').waves
+    expect(analyze.load(waveData[0])).to.equal('knock');
+  })
+
 })
