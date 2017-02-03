8ed3fcd0afe35484e0a8ce7948a65e0d02c70b14
kento sasamoto <knote0721@gmail.com>
2017-02-03 13:02:15 +0900

    gulp browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
 var plumber = require('gulp-plumber');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('browserify', function(){
+  browserify({
+    entries: ['./sample/main.js']
+  })
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(gulp.dest('./sample/'));
+});
 
 gulp.task('babel', function() {
   gulp.src('./src/*.es6')
-    .pipe(babel())
     .pipe(plumber())
+    .pipe(babel())
     .pipe(gulp.dest('./lib/'))
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./src/*.es6', ['babel'])
+  gulp.watch('./src/*.es6', ['babel', 'browserify'])
 });
 
 gulp.task('default', ['babel', 'watch']);
